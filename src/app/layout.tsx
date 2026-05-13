7643dc7a1d98454ded4901a020ecf91ff96b65cb
guilhermerays <guilherme.afonso89@gmail.com>
2026-05-13 10:36:48 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,31 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "Rio Pardo Embalagens | Embalagens, Confeitaria e Descartáveis",
+
   description:
     "A maior variedade em artigos de festa, confeitaria, embalagens e descartáveis em São José do Rio Pardo.",
+
+  openGraph: {
+    title: "Rio Pardo Embalagens",
+
+    description:
+      "Soluções completas em embalagens, descartáveis e confeitaria.",
+
+    url: "https://riopardoembalagens.com.br",
+
+    siteName: "Rio Pardo Embalagens",
+
+    images: [
+      {
+        url: "/fachada.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+
+    locale: "pt_BR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
